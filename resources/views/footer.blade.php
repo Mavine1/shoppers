7dcbc4c1eceea624357544acfce7fd8f31123202
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-25 16:36:00 +0300

    agree opinion fell both soap

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,1 +1,6 @@
-<h1>fgg</h1>
+<div style="clear:both" class="panel panel-default">
+    <div class="panel-body">
+      Panel content
+    </div>
+    <div class="panel-footer">Panel footer</div>
+  </div>
