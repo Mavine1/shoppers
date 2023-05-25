15ed067d22749e49ea84302d444a787e67e74d61
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-25 13:50:01 +0300

    jet victory furniture can unless

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -12,15 +12,12 @@
           <a class="nav-link active" aria-current="page" href="#">Orders</a>
         </li>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav me-auto mb-1 mb-lg-2">
       <li class="nav-item">
           <a class="nav-link" href="#">Cart(0)</a>
         </li>
