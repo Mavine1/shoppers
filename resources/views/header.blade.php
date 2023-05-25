6af92263f4f3dc61ee4a95ae56415cc0b454a1e3
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-25 12:46:01 +0300

    brown joined when industry needle

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -20,7 +20,9 @@
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
-      <ul class="nav navbar-nav navbar></ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Cart(0)</li>
+      </ul>
     </div>
   </div>
 </nav>
