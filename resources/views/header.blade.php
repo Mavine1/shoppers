c652c2a845fba558e73ade04defcc8cbf59590e6
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-25 12:54:00 +0300

    butter arrow mine available unknown

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -16,11 +16,11 @@
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
-      <ul class="nav-item">
+      <div class="nav-item">
       <li class="nav-item">
       <a class="nav-link active" aria-current="page" href="#">Orders</a>
         </li>
-      </ul>
+</div>
     </div>
   </div>
 </nav>
