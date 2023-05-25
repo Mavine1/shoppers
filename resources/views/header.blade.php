19e2278d771b0a881b29f1c88d7cd5ebd3237325
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-25 12:44:00 +0300

    broken closer same goes laid

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -20,6 +20,7 @@
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
+      <ul class="nav navbar-nav navbar></ul>
     </div>
   </div>
 </nav>
