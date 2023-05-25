6721c33a4857d2419c33525a12499cb55e280d76
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-25 12:48:01 +0300

    box if pride rope fifteen

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -20,7 +20,7 @@
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
-      <ul class="nav navbar-nav navbar-right">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li><a href="#">Cart(0)</li>
       </ul>
     </div>
