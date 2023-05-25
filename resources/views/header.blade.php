ec7c5e1637a799026aca32d2bcf4d9ab850acb1e
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-25 12:16:00 +0300

    lower itself cowboy fuel particles

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -13,9 +13,8 @@
         <a class="nav-link" href="#">Order</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-lg-2" type="submit">Search</button>
+    <form class="navbar-form navbar-left">
+      
       <ul class="nav navbar-nav navbar-right">
         <li><a class ="nav-link" href="#">Cart(0)</a></li>
     </ul>
