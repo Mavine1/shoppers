e89320a9bff57febbea4534ff9d600ca855576ad
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-29 23:26:00 +0300

    age hundred wild grabbed problem

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -18,7 +18,7 @@ class userSeeder extends Seeder
         DB::table('users')->insert([
             'name'=>'mavine',
             'email'=>'mavo@gmail.com'
-            'password'=>Hash::make('12345')
-        );
+            'password'=>Hash::make("12345")
+        ]);
     }
 }
