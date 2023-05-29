0c382cb442776bea4b87052acd953a6f9ffab36c
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-29 10:28:08 +0300

    job tobacco tell live vowel

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,8 @@
 @extends('master')
 @section("content")
 <div class="container custom-login">
-    <div class="row">
-        <div class="col-sm-8 col-sm-offset-4">
+    <div class="row ">
+        <div class="col-sm-4 col-sm-offset-4">
         <form>
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
