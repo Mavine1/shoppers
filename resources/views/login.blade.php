dcfc36da224df985e1ae126a103dd313f7741fce
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-29 19:46:01 +0300

    struck length world warn church

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,11 +7,11 @@
   <div class="form-group">
     @csrf
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="email" name="email"  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="mb-2 form-control" id="exampleInputPassword1" placeholder="Password">
+    <input type="password"  name="Password" class="mb-2 form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
   <button type="submit" class="mb-4 btn btn-outline-primary">Login</button>
 </form>
