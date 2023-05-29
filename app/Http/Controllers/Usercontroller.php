9b7e86c251930e3c32f2f89517c5aaf89e82036b
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-29 19:50:01 +0300

    motion organization location second steam

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models
+use App\Models\User;
 class UserController extends Controller
 {
     //
     function login(Request $req)
     {
-        return $req->input();
+        return User::where([''])
     }
 }
