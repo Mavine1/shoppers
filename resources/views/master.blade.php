cc331fc71b0e619c900e0118abf51d95e00673ba
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-29 19:16:00 +0300

    darkness fur sides mill other

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -16,10 +16,8 @@
 </body>
 <style>
     .custom-login{
-    text-align: center;
-    padding: 1.5rem 0;
-    border-radius: 1.5rem 0;
-    border-top: 2px;
+        height: 450px;
+        padding-top: 100px;
     }
     .footer_copyright {
     text-align: center;
