3bcf1cf35151297e3ba396890875b037aaaad242
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-30 10:54:01 +0300

    color believed stared white end

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -10,7 +10,7 @@ class UserController extends Controller
     function login(Request $req)
     {
         $user= User::where(['email'=>$req->email])->first();
-        if($user || Hash::check($req->password,$user->password))
+        if($user || Hash::check($req->username,$user->password))
         {
             return "username or Password is not matched";
         }
