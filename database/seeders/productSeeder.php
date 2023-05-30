73b46b2568b69c809cea8d69aa021663d8052524
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-30 13:04:01 +0300

    energy near policeman atomic wise

--- database/seeders/productSeeder.php
+++ database/seeders/productSeeder.php
@@ -15,38 +15,7 @@ class productSeeder extends Seeder
     {
         //
         DB::table('products')->insert([
-           [
-            'name'=>'Opp Mobile',
-            "price"=>"230",
-            "description"=>"A smartphone with a 4gb ram and much more features",
-            "Category"=>"mobile",
-            "gallery"=>"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tecno-mobile.com%2Fphones%2Fproduct-list%2F&psig=AOvVaw2pLHjC96laKyL1Ke0OAs3G&ust=1685522555353000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC30p3TnP8CFQAAAAAdAAAAABAE",
            
-           ],
-           [
-            'name'=>'Samsung Mobile',
-            "price"=>"250",
-            "description"=>"A smartphone with a 4gb ram and much more features",
-            "Category"=>"mobile",
-            "gallery"=>"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tecno-mobile.com%2Fphones%2Fproduct-list%2F&psig=AOvVaw2pLHjC96laKyL1Ke0OAs3G&ust=1685522555353000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC30p3TnP8CFQAAAAAdAAAAABAE",
-           
-           ],
-           [
-            'name'=>'Tecno Mobile',
-            "price"=>"200",
-            "description"=>"A smartphone with a 4gb ram and much more features",
-            "Category"=>"mobile",
-            "gallery"=>"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tecno-mobile.com%2Fphones%2Fproduct-list%2F&psig=AOvVaw2pLHjC96laKyL1Ke0OAs3G&ust=1685522555353000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC30p3TnP8CFQAAAAAdAAAAABAE",
-           
-           ],
-           [
-            'name'=>'Tecno Mobile',
-            "price"=>"200",
-            "description"=>"A smartphone with a 4gb ram and much more features",
-            "Category"=>"mobile",
-            "gallery"=>"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tecno-mobile.com%2Fphones%2Fproduct-list%2F&psig=AOvVaw2pLHjC96laKyL1Ke0OAs3G&ust=1685522555353000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC30p3TnP8CFQAAAAAdAAAAABAE",
-           
-           ]
         ]);
     }
 }
