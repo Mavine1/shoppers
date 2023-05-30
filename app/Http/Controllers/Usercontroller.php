b425463fa996905f3bc11537b55de81106abb369
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-30 10:04:02 +0300

    arrow crack wolf solve blank

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -15,7 +15,7 @@ class UserController extends Controller
             return "username or Password is not matched";
         }
         else{
-            $req->session()
+            $req->session()->put('user',$user);
             return redirect('/');
         }
     }
