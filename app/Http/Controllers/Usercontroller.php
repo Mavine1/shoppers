ca5b489347eeb8b8eacd6df60591c69eac1335ca
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-30 09:26:03 +0300

    medicine pie different having shorter

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -15,6 +15,7 @@ class UserController extends Controller
             return "username or Password is not matched";
         }
         else{
+            $req
             return redirect('/');
         }
     }
