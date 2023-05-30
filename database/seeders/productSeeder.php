8b6f698100d5910b75c04d9010b9625911581d1e
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-30 11:50:00 +0300

    becoming garden progress recall carried

--- database/seeders/productSeeder.php
+++ database/seeders/productSeeder.php
@@ -15,11 +15,6 @@ class productSeeder extends Seeder
     {
         //
         DB::table('products')->insert([
-            'name'=>'Tecno Mobile',
-            "price"=>"200",
-            "description"=>"A smartphone with a 4gb ram and much more features",
-            "Category"=>"mobile",
-            "galley"=>"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tecno-mobile.com%2Fphones%2Fproduct-list%2F&psig=AOvVaw2pLHjC96laKyL1Ke0OAs3G&ust=1685522555353000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC30p3TnP8CFQAAAAAdAAAAABAE",
            
         ]);
     }
