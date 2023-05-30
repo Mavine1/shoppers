ccd9c4968af43bf46b4b313fa12eaddc7687f48a
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-30 13:58:00 +0300

    tax height railroad moving stomach

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -12,7 +12,6 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- Optional theme -->
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
@@ -30,10 +29,10 @@
         height: 400px !important;
     }
     .custom-product{
-        height: 600px;
+        height: 700px;
     }
     .slider-text{
-        background-color: #35443585 !important;
+        background-color:#1da1f2  !important;
     }
     .trending-image{
         height: 100px;
