24dbd48e01bfdd78f6cdaf5b885f4262f1adf700
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-05-31 10:54:01 +0300

    around round gate window experiment

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\controllers\UserController;
-use App\Http\controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -10,8 +11,8 @@ use App\Http\controllers\ProductController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
@@ -19,6 +20,18 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/logout', function () {
+    Session::forget('user');
+    return redirect('login');
+});
+
 Route::post("/login",[UserController::class,'login']);
 Route::get("/",[ProductController::class,'index']);
-Route::get("/detail/{id}",[ProductController::class,'detail']);
+Route::get("detail/{id}",[ProductController::class,'detail']);
+Route::get("search",[ProductController::class,'search']);
+Route::post("add_to_cart",[ProductController::class,'addToCart']);
+Route::get("cartlist",[ProductController::class,'cartList']); 
+Route::get("removecart/{id}",[ProductController::class,'removeCart']); 
+Route::get("ordernow",[ProductController::class,'orderNow']); 
+Route::post("orderplace",[ProductController::class,'orderPlace']);
+Route::get("myorders",[ProductController::class,'myOrders']);
