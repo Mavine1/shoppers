e915c172cb3d43e0209ed8dee65aab9055ea70a6
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-06-01 10:10:01 +0300

    explore college solve passage belt

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -44,6 +44,7 @@ if(Session::has('user'))
           </li>
           @else
           <li><a href="/login">Login</a></li>
+          <li><a href="/Register">Register</a></li>
           @endif
         </ul>
       </div><!-- /.navbar-collapse -->
